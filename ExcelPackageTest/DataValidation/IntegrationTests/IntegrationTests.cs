12d63a9ce1a2edf3a160bfc37c62f8c2f1c065f0
swmal <swmal@localhost>
2011-04-04 20:52:10 +0000

    added AddressUtility.cs

--- ExcelPackageTest/DataValidation/IntegrationTests/IntegrationTests.cs
+++ ExcelPackageTest/DataValidation/IntegrationTests/IntegrationTests.cs
@@ -97,5 +97,43 @@ namespace ExcelPackageTest.DataValidation.IntegrationTests
                 Assert.AreEqual(3, package.Workbook.Worksheets[1].DataValidations.Count);
             }
         }
+
+        //[TestMethod]
+        //public void Bug_OpenOffice()
+        //{
+        //    var xlsPath = GetTestOutputPath("OpenOffice.xlsx");
+        //    var newFile = new FileInfo(xlsPath);
+        //    if( newFile.Exists)
+        //    {
+        //        newFile.Delete();
+        //        //ensures we create a new workbook
+        //        newFile = new FileInfo(xlsPath);
+
+        //    }
+        //    using(var package = new ExcelPackage(newFile))
+        //    {
+        //        // add a new worksheet to the empty workbook
+        //        var worksheet = package.Workbook.Worksheets.Add("Inventory");
+        //        // Add the headers
+        //        worksheet.Cells[1, 1].Value = "ID";
+        //        worksheet.Cells[1, 2].Value = "Product";
+        ////        worksheet.Cells[1, 3].Value = "Quantity";
+        //        worksheet.Cells[1, 4].Value = "Price";
+
+        //        worksheet.Cells[1, 5].Value = "Value";
+
+        //        worksheet.Column(1).Width = 15;
+        //        worksheet.Column(2).Width = 12;
+        //        worksheet.Column(3).Width = 12;
+
+        //        worksheet.View.PageLayoutView = true;
+
+        //        package.Workbook.Properties.Title = "Invertory";
+        //        package.Workbook.Properties.Author = "Jan Källman";
+        //        package.Save();
+        //    }
+
+
+        }
     }
 }
