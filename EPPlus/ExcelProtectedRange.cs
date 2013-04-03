9e722c877541b0fe981d62a625d01321ec52b88d
tomasz <tomasz@grobelny.oswiecenia.net>
2013-04-03 13:29:30 +0200

    add support for protected ranges

---
+++ EPPlus/ExcelProtectedRange.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OfficeOpenXml
+{
+    public class ExcelProtectedRange
+    {
+        public string Name { get; set; }
+        public ExcelAddress Address { get; set; }
+
+        public ExcelProtectedRange(string name, ExcelAddress address)
+        {
+            Name = name;
+            Address = address;
+        }
+    }
+}
