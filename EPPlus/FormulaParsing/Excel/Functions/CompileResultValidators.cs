93141666acb6ac085b343f1d121c6be72b7c4c7d
Mats Alm <swmal@users.noreply.github.com>
2013-12-25 09:28:45 +0100

    Added validation of compileresult, handling of Ranges in FormulaParser.ParseCell

---
+++ EPPlus/FormulaParsing/Excel/Functions/CompileResultValidators.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions
+{
+    public class CompileResultValidators
+    {
+        private readonly Dictionary<DataType, CompileResultValidator> _validators = new Dictionary<DataType, CompileResultValidator>(); 
+
+        private CompileResultValidator CreateOrGet(DataType dataType)
+        {
+            if (_validators.ContainsKey(dataType))
+            {
+                return _validators[dataType];
+            }
+            if (dataType == DataType.Decimal)
+            {
+                return _validators[DataType.Decimal] = new DecimalCompileResultValidator();
+            }
+            return CompileResultValidator.Empty;
+        }
+
+        public CompileResultValidator GetValidator(DataType dataType)
+        {
+            return CreateOrGet(dataType);
+        }
+    }
+}
