f216baeddbf18247f6b7c8e1d6d9d670d08079a4
Mats Alm <swmal@users.noreply.github.com>
2014-03-09 09:19:55 +0100

    Started work on a winforms/excel demo project.

---
+++ EPPlusExcelFormDemo/ExcelForm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace EPPlusExcelFormDemo
+{
+    partial class ExcelForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl_Worksheets = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView_Ws1 = new System.Windows.Forms.DataGridView();
+            this.A = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.B = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.D = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.E = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_fx = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button_Save = new System.Windows.Forms.Button();
+            this.button_ApplyFormula = new System.Windows.Forms.Button();
+            this.saveFileDialog_SaveExcel = new System.Windows.Forms.SaveFileDialog();
+            this.tabControl_Worksheets.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Ws1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl_Worksheets
+            // 
+            this.tabControl_Worksheets.Controls.Add(this.tabPage1);
+            this.tabControl_Worksheets.Controls.Add(this.tabPage2);
+            this.tabControl_Worksheets.Location = new System.Drawing.Point(13, 63);
+            this.tabControl_Worksheets.Name = "tabControl_Worksheets";
+            this.tabControl_Worksheets.SelectedIndex = 0;
+            this.tabControl_Worksheets.Size = new System.Drawing.Size(583, 386);
+            this.tabControl_Worksheets.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button_ApplyFormula);
+            this.tabPage1.Controls.Add(this.dataGridView_Ws1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox_fx);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(575, 360);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView_Ws1
+            // 
+            this.dataGridView_Ws1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Ws1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.A,
+            this.B,
+            this.C,
+            this.D,
+            this.E});
+            this.dataGridView_Ws1.Location = new System.Drawing.Point(6, 65);
+            this.dataGridView_Ws1.Name = "dataGridView_Ws1";
+            this.dataGridView_Ws1.Size = new System.Drawing.Size(545, 289);
+            this.dataGridView_Ws1.TabIndex = 2;
+            // 
+            // A
+            // 
+            this.A.HeaderText = "A";
+            this.A.Name = "A";
+            // 
+            // B
+            // 
+            this.B.HeaderText = "B";
+            this.B.Name = "B";
+            // 
+            // C
+            // 
+            this.C.HeaderText = "C";
+            this.C.Name = "C";
+            // 
+            // D
+            // 
+            this.D.HeaderText = "D";
+            this.D.Name = "D";
+            // 
+            // E
+            // 
+            this.E.HeaderText = "E";
+            this.E.Name = "E";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "fx";
+            // 
+            // textBox_fx
+            // 
+            this.textBox_fx.Location = new System.Drawing.Point(94, 16);
+            this.textBox_fx.Name = "textBox_fx";
+            this.textBox_fx.Size = new System.Drawing.Size(356, 20);
+            this.textBox_fx.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(575, 360);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button_Save
+            // 
+            this.button_Save.Location = new System.Drawing.Point(17, 12);
+            this.button_Save.Name = "button_Save";
+            this.button_Save.Size = new System.Drawing.Size(95, 23);
+            this.button_Save.TabIndex = 1;
+            this.button_Save.Text = "Save Excelfile";
+            this.button_Save.UseVisualStyleBackColor = true;
+            this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
+            // 
+            // button_ApplyFormula
+            // 
+            this.button_ApplyFormula.Location = new System.Drawing.Point(456, 14);
+            this.button_ApplyFormula.Name = "button_ApplyFormula";
+            this.button_ApplyFormula.Size = new System.Drawing.Size(64, 23);
+            this.button_ApplyFormula.TabIndex = 3;
+            this.button_ApplyFormula.Text = "Apply";
+            this.button_ApplyFormula.UseVisualStyleBackColor = true;
+            this.button_ApplyFormula.Click += new System.EventHandler(this.button_ApplyFormula_Click);
+            // 
+            // ExcelForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 461);
+            this.Controls.Add(this.button_Save);
+            this.Controls.Add(this.tabControl_Worksheets);
+            this.Name = "ExcelForm";
+            this.Text = "EPPlus Excel demo";
+            this.tabControl_Worksheets.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Ws1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl_Worksheets;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView_Ws1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn A;
+        private System.Windows.Forms.DataGridViewTextBoxColumn B;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C;
+        private System.Windows.Forms.DataGridViewTextBoxColumn D;
+        private System.Windows.Forms.DataGridViewTextBoxColumn E;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_fx;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button_Save;
+        private System.Windows.Forms.Button button_ApplyFormula;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog_SaveExcel;
+    }
+}
+
---
+++ EPPlusExcelFormDemo/ExcelForm.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+namespace EPPlusExcelFormDemo
+{
+    public partial class ExcelForm : Form
+    {
+        private ExcelPackage _package;
+        private DataGridViewCell _currentCell;
+        private Font _inactiveCellFont;
+        private Font _activeCellFont;
+
+        public ExcelForm()
+        {
+            InitializeComponent();
+            InitializePackage();
+            InitPackageToUI();
+            this.Closing += (sender, args) => _package.Dispose();
+        }
+
+        private void InitializePackage()
+        {
+            _package = new ExcelPackage(new MemoryStream());
+            var ws1 = _package.Workbook.Worksheets.Add("Worksheet1");
+            for (var col = 2; col < 5; col++)
+            {
+                for (var row = 1; row < 9; row++)
+                {
+                    ws1.Cells[row, col].Value = row*col;
+                }
+            }
+            ws1.Cells[7, 1].Value = "SUM";
+            ws1.Cells[7, 2].Formula = "SUM(B1:B6)";
+            ws1.Cells[7, 3].Formula = "SUM(C1:C6)";
+            ws1.Cells[7, 4].Formula = "SUM(D1:D6)";
+
+            ws1.Cells[8, 1].Value = "STDEV";
+            ws1.Cells[8, 2].Formula = "STDEV(B1:B6)";
+            ws1.Cells[8, 3].Formula = "STDEV(C1:C6)";
+            ws1.Cells[8, 4].Formula = "STDEV(D1:D6)";
+            _package.Workbook.Calculate();
+        }
+
+        private void InitFonts(DataGridView gridView)
+        {
+            _activeCellFont = new Font(gridView.Font, FontStyle.Bold);
+            _inactiveCellFont = gridView.Font;
+        }
+
+        private void InitEvents(DataGridView gridView)
+        {
+            gridView.CellEnter += DataGrid1OnCellEnter;
+            gridView.CellLeave += DataGrid1OnCellLeave; 
+        }
+
+        /// <summary>
+        /// Binds the EPPlus package (or actually only its first worksheet)
+        /// to the DataGridView.
+        /// </summary>
+        private void InitPackageToUI()
+        {
+            var ws = _package.Workbook.Worksheets.First();
+            var page1 = this.tabControl_Worksheets.Controls[0] as TabPage;
+            page1.Text = ws.Name;
+            var gridView = GetGrid();
+            InitFonts(gridView);
+            InitEvents(gridView);
+            
+            for (var row = 0; row < ws.Dimension.Rows; row++)
+            {
+                var gridRow = new DataGridViewRow {HeaderCell = {Value = (row + 1).ToString()}};
+                for (var col = 0; col < 5; col++)
+                {
+                    var cell = ws.Cells[row + 1, col + 1];
+                    using (var uiCell = new DataGridViewTextBoxCell())
+                    {
+                        uiCell.Value = cell.Value;
+                        gridRow.Cells.Add(uiCell);
+                    }
+                }
+                gridView.Rows.Add(gridRow);
+            }
+            gridView.Refresh();
+        }
+
+        private void BindPackageToUI()
+        {
+            var dataGrid1 = GetGrid();
+            for (var row = 1; row < _package.Workbook.Worksheets.First().Dimension.Rows; row++)
+            {
+                for (var col = 1; col < 5; col++)
+                {
+                    var excelCell = _package.Workbook.Worksheets.First().Cells[row, col];
+                    var gridViewCell = dataGrid1.Rows[row - 1].Cells[col - 1];
+                    gridViewCell.Value = excelCell.Value;
+                }
+            }
+            dataGrid1.Refresh();
+        }
+
+        private object CellValueToObject(string cellVal)
+        {
+            if (ConvertUtil.IsNumericString(cellVal))
+            {
+                return double.Parse(cellVal, CultureInfo.InvariantCulture);
+            }
+            return cellVal;
+        }
+
+        private void DataGrid1OnCellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            var dataGrid1 = GetGrid();
+            var gridViewCell = dataGrid1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            var excelCell = _package.Workbook.Worksheets.First().Cells[e.RowIndex + 1, e.ColumnIndex + 1];
+            gridViewCell.Style.ForeColor = Color.Black;
+            gridViewCell.Style.BackColor = Color.White;
+            gridViewCell.Style.Font = _inactiveCellFont;
+
+
+            var result = gridViewCell.EditedFormattedValue.ToString();
+            
+            if (result.StartsWith("="))
+            {
+                excelCell.Formula = result.Substring(1);
+            }
+            else if(textBox_fx.Text.StartsWith("="))
+            {
+                excelCell.Formula = textBox_fx.Text.Substring(1);
+            }
+            else
+            {
+                excelCell.Value = CellValueToObject(result);
+            }
+            _package.Workbook.Calculate();
+            BindPackageToUI();
+            dataGrid1.Refresh();
+        }
+
+
+        private DataGridView GetGrid()
+        {
+            var page1 = this.tabControl_Worksheets.Controls[0] as TabPage;
+            var dataGrid1 = page1.Controls["dataGridView_Ws1"] as DataGridView;
+            return dataGrid1;
+        }
+
+        private void DataGrid1OnCellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            var dataGrid1 = GetGrid();
+            dataGrid1.Refresh();
+            var cell = dataGrid1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            var excelCell = _package.Workbook.Worksheets.First().Cells[e.RowIndex + 1, e.ColumnIndex + 1];
+            if (!string.IsNullOrEmpty(excelCell.Formula))
+            {
+                textBox_fx.Text = "=" + excelCell.Formula;
+            }
+            else if(excelCell.Value != null)
+            {
+                textBox_fx.Text = excelCell.Value.ToString();
+            }
+            cell.Style.ForeColor = Color.Blue;
+            cell.Style.BackColor = Color.Gainsboro;
+            cell.Style.Font = _activeCellFont;
+            _currentCell = cell;
+        }
+
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+            var dialogResult = saveFileDialog_SaveExcel.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                _package.SaveAs(new FileInfo(saveFileDialog_SaveExcel.FileName));
+            }
+        }
+
+        private void button_ApplyFormula_Click(object sender, EventArgs e)
+        {
+            var row = _currentCell.RowIndex;
+            var col = _currentCell.ColumnIndex;
+            var txt = textBox_fx.Text;
+            if (txt.StartsWith("="))
+            {
+                _package.Workbook.Worksheets.First().Cells[row + 1, col + 1].Formula = txt.Substring(1);
+            }
+            else
+            {
+                _package.Workbook.Worksheets.First().Cells[row + 1, col + 1].Value = CellValueToObject(txt);
+            }
+            _package.Workbook.Calculate();
+            BindPackageToUI();
+            this.Refresh();
+        }
+    }
+}
