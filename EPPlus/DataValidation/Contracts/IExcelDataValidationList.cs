56a9ccf409f83558bca1de138817fa18be631ae2
JanKallman <JanKallman@users.noreply.github.com>
2014-04-15 21:37:55 +0200

    Fixed bug when using RepeatColumns & RepeatRows at the same time. VBA project will be left untouched if its not accessed. Fixed problem with strings on save. Added locks to the cellstore for access by mulitple threads.

---
+++ EPPlus/DataValidation/Contracts/IExcelDataValidationList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OfficeOpenXml.DataValidation.Formulas.Contracts;
+
+namespace OfficeOpenXml.DataValidation.Contracts
+{
+    public interface IExcelDataValidationList : IExcelDataValidationWithFormula<IExcelDataValidationFormulaList>
+    {
+    }
+}
