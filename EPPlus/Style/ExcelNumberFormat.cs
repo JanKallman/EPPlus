c801787539d06f25e7ceee962e63526c73297b11
JanKallman <JanKallman@localhost>
2014-05-06 19:55:50 +0200

    Fixed issue 14976 Incorrect builtin numberformat 40

--- EPPlus/Style/ExcelNumberFormat.cs
+++ EPPlus/Style/ExcelNumberFormat.cs
@@ -144,7 +144,7 @@ namespace OfficeOpenXml.Style
                 case 39:
                     return "#,##0.00;(#,##0.00)";
                 case 40:
-                    return "#,##0.00;[Red](#,#)";
+                    return "#,##0.00;[Red](#,##0.00)";
                 case 45:
                     return "mm:ss";
                 case 46:
@@ -208,7 +208,7 @@ namespace OfficeOpenXml.Style
                     return 38;
                 case "#,##0.00;(#,##0.00)":
                     return 39;
-                case "#,##0.00;[Red](#,#)":
+                case "#,##0.00;[Red](#,##0.00)":
                     return 40;
                 case "mm:ss":
                     return 45;
