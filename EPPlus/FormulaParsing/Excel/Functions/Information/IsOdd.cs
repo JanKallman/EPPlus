20f6c7f890241751ad1ab2975d50d009e21e42f6
JanKallman <JanKallman@users.noreply.github.com>
2014-05-06 19:54:57 +0200

    Changed namespace for DotNetZip classes to OfficeOpenXml.Packaging.Ionic.Crc to avoid problems when using Dotnetzip. Fixed issue 14995 (style problem when Normal style is not the first named style) Fixed issue 14991 Row Pagebreaks Fixed issue 14992 Corrupt arrayformulas. Fixed issue 14982 Ambiguous class Ionic.Zip.BadPasswordException. Fixed issue 14975 XYScatterSmooth Chart Error Fixed issue 14976 Incorrect builtin numberformat 40 Fixed issue 14811 VBA compression error (Owerflow in buffer in CompressChunk in Componddocument).

---
+++ EPPlus/FormulaParsing/Excel/Functions/Information/IsOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+using OfficeOpenXml.Utils;
+
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Information
+{
+    public class IsOdd : ExcelFunction
+    {
+        public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
+        {
+            ValidateArguments(arguments, 1);
+            var arg1 = GetFirstValue(arguments);//arguments.ElementAt(0);
+            if (!ConvertUtil.IsNumeric(arg1))
+            {
+                ThrowExcelErrorValueException(eErrorType.Value);
+            }
+            var number = (int)System.Math.Floor(ConvertUtil.GetValueDouble(arg1));
+            return CreateResult(number % 2 == 1, DataType.Boolean);
+        }
+    }
+}
---
+++ EPPlus/Utils/AddressUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace OfficeOpenXml.Utils
+{
+    public static class AddressUtility
+    {
+        public static string ParseEntireColumnSelections(string address)
+        {
+            string parsedAddress = address;
+            var matches = Regex.Matches(address, "[A-Z]+:[A-Z]+");
+            foreach (Match match in matches)
+            {
+                AddRowNumbersToEntireColumnRange(ref parsedAddress, match.Value);
+            }
+            return parsedAddress;
+        }
+
+        private static void AddRowNumbersToEntireColumnRange(ref string address, string range)
+        {
+            var parsedRange = string.Format("{0}{1}", range, ExcelPackage.MaxRows);
+            var splitArr = parsedRange.Split(new char[] { ':' });
+            address = address.Replace(range, string.Format("{0}1:{1}", splitArr[0], splitArr[1]));
+        }
+    }
+}
