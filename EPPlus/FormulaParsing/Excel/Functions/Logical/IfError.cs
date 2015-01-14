0b1b991c1e642598e45d7ed4e91d4a587bb19a1f
Mats Alm <swmal@users.noreply.github.com>
2015-01-14 22:51:21 +0100

    Implemented IfError function.

---
+++ EPPlus/FormulaParsing/Excel/Functions/Logical/IfError.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Logical
+{
+    public class IfError : ExcelFunction
+    {
+        public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
+        {
+            ValidateArguments(arguments, 1);
+            var firstArg = arguments.First();
+            return GetResultByObject(firstArg.Value);
+        }
+
+        private CompileResult GetResultByObject(object result)
+        {
+            if (IsNumeric(result))
+            {
+                return CreateResult(result, DataType.Decimal);
+            }
+            if (result is string)
+            {
+                return CreateResult(result, DataType.String);
+            }
+            if (ExcelErrorValue.Values.IsErrorValue(result))
+            {
+                return CreateResult(result, DataType.ExcelAddress);
+            }
+            if (result == null)
+            {
+                return CompileResult.Empty;
+            }
+            return CreateResult(result, DataType.Enumerable);
+        }
+    }
+}
---
+++ EPPlus/FormulaParsing/ExpressionGraph/FunctionCompilers/IfErrorFunctionCompiler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OfficeOpenXml.FormulaParsing.Excel.Functions;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Logical;
+using OfficeOpenXml.FormulaParsing.Exceptions;
+using OfficeOpenXml.FormulaParsing.Utilities;
+
+namespace OfficeOpenXml.FormulaParsing.ExpressionGraph.FunctionCompilers
+{
+    public class IfErrorFunctionCompiler : FunctionCompiler
+    {
+        public IfErrorFunctionCompiler(IfError function)
+            : base(function)
+        {
+            Require.That(function).Named("function").IsNotNull();
+          
+        }
+
+        public override CompileResult Compile(IEnumerable<Expression> children, ParsingContext context)
+        {
+            if (children.Count() != 2) throw new ExcelErrorValueException(eErrorType.Value);
+            var args = new List<FunctionArgument>();
+            Function.BeforeInvoke(context);
+            var firstChild = children.First();
+            var lastChild = children.ElementAt(1);
+            try
+            {
+                var result = firstChild.Compile();
+                if (result.DataType == DataType.ExcelError)
+                {
+                    args.Add(new FunctionArgument(lastChild.Compile().Result));
+                }
+                else
+                {
+                    args.Add(new FunctionArgument(result.Result)); 
+                }
+                
+            }
+            catch (ExcelErrorValueException ex)
+            {
+                args.Add(new FunctionArgument(lastChild.Compile().Result));
+            }
+            return Function.Execute(args, context);
+        }
+    }
+}
