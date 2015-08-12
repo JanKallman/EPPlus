97efcfdc81cea0e8135f4412d787a55b624918c2
Justin Dearing <zippy1981@gmail.com>
2015-08-12 00:09:56 -0400

    Refactored unit tests to make them more independent

--- EPPlusTest/TestBase.cs
+++ EPPlusTest/TestBase.cs
@@ -2,17 +2,21 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OfficeOpenXml;
 using System.IO;
+using System.Linq;
 
 namespace EPPlusTest
 {
     [TestClass]
-    public class TestBase
+    public abstract class TestBase
     {
         protected ExcelPackage _pck;        
         protected string _worksheetPath="";
         protected string _worksheetName = "";
+        private string _clipArtPath = null;
         public TestContext TestContext { get; set; }
-        protected void InitBase()
+        
+        [TestInitialize]
+        public void InitBase()
         {
 
             _worksheetPath = AppDomain.CurrentDomain.BaseDirectory + @"\..\..\worksheets";
@@ -39,6 +43,25 @@ namespace EPPlusTest
                 fi.Delete();
             }
             _pck.SaveAs(fi);
+        }
+
+        /// <returns>The path to the Microsoft Office Clipart directory or an empty string if that isn't found.</returns>
+        [Obsolete("Create some clipart")]
+        protected string GetClipartPath()
+        {
+            if (_clipArtPath != null) return _clipArtPath;
+            _clipArtPath = "";
+            var basePaths = new[]
+            {
+                Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
+                Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86),
+            };
+            foreach (var path in basePaths.Select(basePath => Path.Combine(basePath, @"Microsoft Office\CLIPART\PUB60COR")).Where(path => Directory.Exists(path)))
+            {
+                _clipArtPath = path;
+                break;
+            }
+            return _clipArtPath;
         }
     }
 }
