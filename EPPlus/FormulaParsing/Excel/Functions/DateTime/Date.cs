9ae5e07040a1abdb16ea28d94b76057657e0ad70
kazy111 <kazy111@outlook.jp>
2016-06-14 19:53:00 +0900

    Fixed memory leak: Release MemoryStream buffer after loading worksheet XML

---
+++ EPPlus/FormulaParsing/Excel/Functions/DateTime/Date.cs
@@ -0,0 +1,48 @@
+﻿/* Copyright (C) 2011  Jan Källman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+ * If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+ *
+ * All code and executables are provided "as is" with no warranty either express or implied. 
+ * The author accepts no liability for any damage or loss of business that this product may cause.
+ *
+ * Code change notes:
+ * 
+ * Author							Change						Date
+ *******************************************************************************
+ * Mats Alm   		                Added		                2013-12-03
+ *******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
+{
+    public class Date : ExcelFunction
+    {
+        public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
+        {
+            ValidateArguments(arguments, 3);
+            var year = ArgToInt(arguments, 0);
+            var month = ArgToInt(arguments, 1);
+            var day = ArgToInt(arguments, 2);
+            var date = new System.DateTime(year, 1, 1);
+            month -= 1;
+            date = date.AddMonths(month);
+            date = date.AddDays((double)(day - 1));
+            return CreateResult(date.ToOADate(), DataType.Date);
+        }
+    }
+}
---
+++ EPPlus/FormulaParsing/Excel/Functions/Math/SumProduct.cs
@@ -0,0 +1,110 @@
+﻿/* Copyright (C) 2011  Jan Källman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+ * If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+ *
+ * All code and executables are provided "as is" with no warranty either express or implied. 
+ * The author accepts no liability for any damage or loss of business that this product may cause.
+ *
+ * Code change notes:
+ * 
+ * Author							Change						Date
+ *******************************************************************************
+ * Mats Alm   		                Added		                2013-12-03
+ *******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OfficeOpenXml.FormulaParsing.Exceptions;
+using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Math
+{
+    public class SumProduct : ExcelFunction
+    {
+        public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
+        {
+            ValidateArguments(arguments, 1);
+            double result = 0d;
+            List<List<double>> results = new List<List<double>>();
+            foreach (var arg in arguments)
+            {
+                results.Add(new List<double>());
+                var currentResult = results.Last();
+                if (arg.Value is IEnumerable<FunctionArgument>)
+                {
+                    foreach (var val in (IEnumerable<FunctionArgument>)arg.Value)
+                    {
+                        AddValue(val.Value, currentResult);
+                    }
+                }
+                else if (arg.Value is FunctionArgument)
+                {
+                    AddValue(arg.Value, currentResult);
+                }
+                else if (arg.IsExcelRange)
+                {
+                    var r=arg.ValueAsRangeInfo;
+                    for (int col = r.Address._fromCol; col <= r.Address._toCol; col++)
+                    {
+                        for (int row = r.Address._fromRow; row <= r.Address._toRow; row++)
+                        {
+                            AddValue(r.GetValue(row,col), currentResult);
+                        }
+                    }
+                }
+                else if(IsNumeric(arg.Value))
+                {
+                    AddValue(arg.Value, currentResult);
+                }
+            }
+            // Validate that all supplied lists have the same length
+            var arrayLength = results.First().Count;
+            foreach (var list in results)
+            {
+                if (list.Count != arrayLength)
+                {
+                    throw new ExcelErrorValueException(ExcelErrorValue.Create(eErrorType.Value));
+                    //throw new ExcelFunctionException("All supplied arrays must have the same length", ExcelErrorCodes.Value);
+                }
+            }
+            for (var rowIndex = 0; rowIndex < arrayLength; rowIndex++)
+            {
+                double rowResult = 1;
+                for (var colIndex = 0; colIndex < results.Count; colIndex++)
+                {
+                    rowResult *= results[colIndex][rowIndex];
+                }
+                result += rowResult;
+            }
+            return CreateResult(result, DataType.Decimal);
+        }
+
+        private void AddValue(object convertVal, List<double> currentResult)
+        {
+            if (IsNumeric(convertVal))
+            {
+                currentResult.Add(Convert.ToDouble(convertVal));
+            }
+            else if (convertVal is ExcelErrorValue)
+            {
+                throw (new ExcelErrorValueException((ExcelErrorValue)convertVal));
+            }
+            else
+            {
+                currentResult.Add(0d);
+            }
+        }
+    }
+}
