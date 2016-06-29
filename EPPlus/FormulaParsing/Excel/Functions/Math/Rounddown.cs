e3c4bd6cee2366a1c81e8792b7e079d5f31c38b0
Mats Alm <swmal@users.noreply.github.com>
2016-06-30 00:10:54 +0200

    Support for dates in Expression evaluation

---
+++ EPPlus/FormulaParsing/Excel/Functions/Math/Rounddown.cs
@@ -0,0 +1,67 @@
+﻿/* Copyright (C) 2011  Jan Källman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+ * If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+ *
+ * All code and executables are provided "as is" with no warranty either express or implied. 
+ * The author accepts no liability for any damage or loss of business that this product may cause.
+ *
+ * Code change notes:
+ * 
+ * Author							Change						Date
+ *******************************************************************************
+ * Mats Alm   		                Added		                2014-01-06
+ *******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Math
+{
+    public class Rounddown : ExcelFunction
+    {
+        public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
+        {
+            ValidateArguments(arguments, 2);
+            var number = ArgToDecimal(arguments, 0);
+            var nDecimals = ArgToInt(arguments, 1);
+
+            var nFactor = number < 0 ? -1 : 1;
+            number *= nFactor;
+
+            double result;
+            if (nDecimals > 0)
+            {
+                result = RoundDownDecimalNumber(number, nDecimals);
+            }
+            else
+            {
+                result = (int)System.Math.Floor(number);
+                result = result - (result % System.Math.Pow(10, (nDecimals*-1)));
+            }
+            return CreateResult(result * nFactor, DataType.Decimal);
+        }
+
+        private static double RoundDownDecimalNumber(double number, int nDecimals)
+        {
+            var integerPart = System.Math.Floor(number);
+            var decimalPart = number - integerPart;
+            decimalPart = System.Math.Pow(10d, nDecimals)*decimalPart;
+            decimalPart = System.Math.Truncate(decimalPart)/System.Math.Pow(10d, nDecimals);
+            var result = integerPart + decimalPart;
+            return result;
+        }
+    }
+}
---
+++ EPPlusTest/FormulaParsing/IntegrationTests/BuiltInFunctions/InformationFunctionsTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using Rhino.Mocks;
+using OfficeOpenXml.FormulaParsing;
+using OfficeOpenXml.FormulaParsing.Exceptions;
+
+namespace EPPlusTest.FormulaParsing.IntegrationTests.BuiltInFunctions
+{
+    [TestClass]
+    public class InformationFunctionsTests : FormulaParserTestBase
+    {
+        private ExcelDataProvider _excelDataProvider;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _excelDataProvider = MockRepository.GenerateStub<ExcelDataProvider>();
+            _parser = new FormulaParser(_excelDataProvider);
+        }
+
+        [TestMethod]
+        public void IsBlankShouldReturnCorrectValue()
+        {
+            var result = _parser.Parse("ISBLANK(A1)");
+            Assert.IsTrue((bool)result);
+        }
+
+        [TestMethod]
+        public void IsNumberShouldReturnCorrectValue()
+        {
+            var result = _parser.Parse("ISNUMBER(10/2)");
+            Assert.IsTrue((bool)result);
+        }
+
+        [TestMethod]
+        public void IsErrorShouldReturnTrueWhenDivBy0()
+        {
+            var result = _parser.Parse("ISERROR(10/0)");
+            Assert.IsTrue((bool)result);
+        }
+
+        [TestMethod]
+        public void IsTextShouldReturnTrueWhenReferencedCellContainsText()
+        {
+            using(var pck = new ExcelPackage())
+            {
+                var sheet = pck.Workbook.Worksheets.Add("Test");
+                sheet.Cells["A1"].Value = "Abc";
+                sheet.Cells["A2"].Formula = "ISTEXT(A1)";
+                sheet.Calculate();
+                var result = sheet.Cells["A2"].Value;
+                Assert.IsTrue((bool)result);
+            }
+        }
+
+        [TestMethod]
+        public void IsErrShouldReturnFalseIfErrorCodeIsNa()
+        {
+            using (var pck = new ExcelPackage())
+            {
+                var sheet = pck.Workbook.Worksheets.Add("Test");
+                sheet.Cells["A1"].Value = ExcelErrorValue.Parse("#N/A");
+                sheet.Cells["A2"].Formula = "ISERR(A1)";
+                sheet.Calculate();
+                var result = sheet.Cells["A2"].Value;
+                Assert.IsFalse((bool)result);
+            }
+        }
+
+        [TestMethod]
+        public void IsNaShouldReturnTrueCodeIsNa()
+        {
+            using (var pck = new ExcelPackage())
+            {
+                var sheet = pck.Workbook.Worksheets.Add("Test");
+                sheet.Cells["A1"].Value = ExcelErrorValue.Parse("#N/A");
+                sheet.Cells["A2"].Formula = "ISNA(A1)";
+                sheet.Calculate();
+                var result = sheet.Cells["A2"].Value;
+                Assert.IsTrue((bool)result);
+            }
+        }
+
+        [TestMethod]
+        public void ErrorTypeShouldReturnCorrectErrorCodes()
+        {
+            using (var pck = new ExcelPackage())
+            {
+                var sheet = pck.Workbook.Worksheets.Add("Test");
+                sheet.Cells["A1"].Value = ExcelErrorValue.Create(eErrorType.Null);
+                sheet.Cells["B1"].Formula = "ERROR.TYPE(A1)";
+                sheet.Cells["A2"].Value = ExcelErrorValue.Create(eErrorType.Div0);
+                sheet.Cells["B2"].Formula = "ERROR.TYPE(A2)";
+                sheet.Cells["A3"].Value = ExcelErrorValue.Create(eErrorType.Value);
+                sheet.Cells["B3"].Formula = "ERROR.TYPE(A3)";
+                sheet.Cells["A4"].Value = ExcelErrorValue.Create(eErrorType.Ref);
+                sheet.Cells["B4"].Formula = "ERROR.TYPE(A4)";
+                sheet.Cells["A5"].Value = ExcelErrorValue.Create(eErrorType.Name);
+                sheet.Cells["B5"].Formula = "ERROR.TYPE(A5)";
+                sheet.Cells["A6"].Value = ExcelErrorValue.Create(eErrorType.Num);
+                sheet.Cells["B6"].Formula = "ERROR.TYPE(A6)";
+                sheet.Cells["A7"].Value = ExcelErrorValue.Create(eErrorType.NA);
+                sheet.Cells["B7"].Formula = "ERROR.TYPE(A7)";
+                sheet.Cells["A8"].Value = 10;
+                sheet.Cells["B8"].Formula = "ERROR.TYPE(A8)";
+                sheet.Calculate();
+                var nullResult = sheet.Cells["B1"].Value;
+                var div0Result = sheet.Cells["B2"].Value;
+                var valueResult = sheet.Cells["B3"].Value;
+                var refResult = sheet.Cells["B4"].Value;
+                var nameResult = sheet.Cells["B5"].Value;
+                var numResult = sheet.Cells["B6"].Value;
+                var naResult = sheet.Cells["B7"].Value;
+                var noErrorResult = sheet.Cells["B8"].Value;
+                Assert.AreEqual(1, nullResult, "Null error was not 1");
+                Assert.AreEqual(2, div0Result, "Div0 error was not 2");
+                Assert.AreEqual(3, valueResult, "Value error was not 3");
+                Assert.AreEqual(4, refResult, "Ref error was not 4");
+                Assert.AreEqual(5, nameResult, "Name error was not 5");
+                Assert.AreEqual(6, numResult, "Num error was not 6");
+                Assert.AreEqual(7, naResult, "NA error was not 7");
+                Assert.AreEqual(ExcelErrorValue.Create(eErrorType.NA), noErrorResult, "No error did not return N/A error");
+            }
+        }
+    }
+}
