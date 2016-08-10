c56309349d520cdc82dd9ba1dd00eba302c46c8b
Motley <Motley@localhost>
2016-08-10 13:06:01 -0500

    Fix Round when second argument is negative

---
+++ EPPlusTest/FormulaParsing/Excel/Functions/RefAndLookup/ChooseTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using EPPlusTest.FormulaParsing.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using OfficeOpenXml.FormulaParsing;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.RefAndLookup;
+using OfficeOpenXml.FormulaParsing.Exceptions;
+
+namespace EPPlusTest.FormulaParsing.Excel.Functions.RefAndLookup
+{
+    [TestClass]
+    public class ChooseTests
+    {
+        private ParsingContext _parsingContext;
+        private ExcelPackage _package;
+        private ExcelWorksheet _worksheet;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _parsingContext = ParsingContext.Create();
+            _package = new ExcelPackage(new MemoryStream());
+            _worksheet = _package.Workbook.Worksheets.Add("test");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _package.Dispose();
+        }
+
+        [TestMethod]
+        public void ChooseSingleValue()
+        {
+            fillChooseOptions();
+            _worksheet.Cells["B1"].Formula = "CHOOSE(4, A1, A2, A3, A4, A5)";
+            _worksheet.Calculate();
+
+            Assert.AreEqual("5", _worksheet.Cells["B1"].Value);
+        }
+
+        [TestMethod]
+        public void ChooseSingleFormula()
+        {
+            fillChooseOptions();
+            _worksheet.Cells["B1"].Formula = "CHOOSE(6, A1, A2, A3, A4, A5, A6)";
+            _worksheet.Calculate();
+
+            Assert.AreEqual("12", _worksheet.Cells["B1"].Value);
+        }
+
+        [TestMethod]
+        public void ChooseMultipleValues()
+        {
+            fillChooseOptions();
+            _worksheet.Cells["B1"].Formula = "SUM(CHOOSE({1,3,4}, A1, A2, A3, A4, A5))";
+            _worksheet.Calculate();
+
+            Assert.AreEqual(9D, _worksheet.Cells["B1"].Value);
+        }
+
+        [TestMethod]
+        public void ChooseValueAndFormula()
+        {
+            fillChooseOptions();
+            _worksheet.Cells["B1"].Formula = "SUM(CHOOSE({2,6}, A1, A2, A3, A4, A5, A6))";
+            _worksheet.Calculate();
+
+            Assert.AreEqual(14D, _worksheet.Cells["B1"].Value);
+        }
+
+        private void fillChooseOptions()
+        {
+            _worksheet.Cells["A1"].Value = 1d;
+            _worksheet.Cells["A2"].Value = 2d;
+            _worksheet.Cells["A3"].Value = 3d;
+            _worksheet.Cells["A4"].Value = 5d;
+            _worksheet.Cells["A5"].Value = 7d;
+            _worksheet.Cells["A6"].Formula = "A4 + A5";
+        }
+    }
+}
---
+++ EPPlusTest/FormulaParsing/ExpressionGraph/DecimalExpressionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml.FormulaParsing.Excel.Operators;
+using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+
+namespace EPPlusTest.FormulaParsing.ExpressionGraph
+{
+    [TestClass]
+    public class DecimalExpressionTests
+    {
+        //[TestMethod]
+        //public void CompileShouldHandlePercent()
+        //{
+        //    var exp1 = new DecimalExpression("1");
+        //    exp1.Operator = Operator.Percent;
+        //    exp1.Next = ConstantExpressions.Percent;
+        //    var result = exp1.Compile();
+        //    Assert.AreEqual(0.01, result.Result);
+        //}
+    }
+}
