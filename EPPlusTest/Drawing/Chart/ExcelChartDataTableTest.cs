0607b2e9d0bbc686887e5b59a860aa987dbae4de
mkromis <mkromis@outlook.com>
2017-01-08 13:43:01 -0500

    Improve test to verify xml is created correctly. Fix chart series so tick labels display correctly.

--- EPPlusTest/Drawing/Chart/ExcelChartDataTableTest.cs
+++ EPPlusTest/Drawing/Chart/ExcelChartDataTableTest.cs
@@ -22,7 +22,6 @@ namespace EPPlusTest.Drawing.Chart
         /// Basic test to check output with excel. need enhanced to be stand alone checking
         /// </summary>
         [TestMethod]
-        [Ignore]
         public void DataTableFile()
         {
             string outfile = Path.Combine(TestContext.TestResultsDirectory, "DataTableFile.xlsx");
@@ -32,20 +31,29 @@ namespace EPPlusTest.Drawing.Chart
                 // Add worksheet with sample data
                 var worksheet = pkg.Workbook.Worksheets.Add("TestData");
                 worksheet.Cells["A1"].Value = "Data";
+                worksheet.Cells["B1"].Value = "Values";
                 for (int x = 1; x < 12; ++x)
                 {
-                    worksheet.Cells[x+1, 1].Value = (double)x / 3.0;
+
+                    worksheet.Cells[x + 1, 1].Value = $"Sample {x}";
+                    worksheet.Cells[x + 1, 2].Value = (double)x / 3.0;
                 }
 
                 // Add chart from sample data
                 var chartsheet = pkg.Workbook.Worksheets.AddChart("TestChart", eChartType.Line);
                 var chart = chartsheet.Chart as ExcelLineChart;
-                chart.Series.Add(worksheet.Cells["A1:A12"], worksheet.Cells["A1:A12"]).Header = "Data Test";
+                chart.Series.Add(worksheet.Cells["B2:B12"], worksheet.Cells["A2:A12"]).Header = "Data Test";
 
                 // as per epplus style, data table does not init until used. so call something
-                chart.PlotArea.DataTable.ShowKeys = true;
+                chart.PlotArea.DataTable.ToString();
 
                 pkg.Save();
+
+                XmlDocument xmldoc = chart.ChartXml;
+                string xml = xmldoc.InnerXml;
+                TestContext.WriteLine(xml);
+                Assert.IsTrue(xml.Contains("c:dTable"));
+                Assert.IsTrue(xml.Contains("/c:dTable"));
             }
         }
     }
