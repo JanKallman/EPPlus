a1ed1661928ab08206309aba41f294829332ecfa
Stenrud <truls.stenrud@gmail.com>
2017-11-01 14:12:21 +1000

    If black and white is not present as srgbColor, add them manually

--- EPPlus/ExcelTheme.cs
+++ EPPlus/ExcelTheme.cs
@@ -27,34 +27,28 @@ namespace OfficeOpenXml
         public IEnumerable<string> Colors => _colors;
 
 
-        // Sometimes black and white is stored as: 
-        //<a:sysClr lastClr = "000000" val="windowText"/>
-        //<a:sysClr lastClr = "FFFFFF" val="window"/>
 
-        //And sometimes as:
-        //<a:srgbClr val = "000000" />
-        //<a:srgbClr val = "FFFFFF" />
-
-        // but in GUI they are always the first two colors
         private void LoadFromDocument()
         {
-            var nodes = _themeXml.GetElementsByTagName("a:srgbClr");
 
-            _colors = new List<string>
+            var themeColorNodes = _themeXml.GetElementsByTagName("a:srgbClr");
+
+            _colors = new List<string>();
+
+            if (themeColorNodes.Count == 10)
             {
-                "FFFFFF",
-                "000000"
-            };
-
-            foreach (XmlElement n in nodes)
-            {
-                string hex = n.GetAttribute("val");
-
-                if (hex != "FFFFFF" && hex != "000000")
-                    _colors.Add(hex);
+                _colors.Add("000000");
+                _colors.Add("FFFFFF");
             }
 
-            // In GUI, color 2 and 3 has switched position, compared to the xml file 
+            foreach (XmlElement n in themeColorNodes)
+            {
+                string hex = n.GetAttribute("val");
+                _colors.Add("FFFFFF");
+            }
+
+            // In GUI, color 0 and 1, and 2 and 3 has switched position, compared to the xml file 
+            _colors.Reverse(0, 2);
             _colors.Reverse(2, 2);
         }
     }
